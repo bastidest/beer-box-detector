39f7818a235860e0c137f7cb089f4e9c75e54b15
Basti Destruction <sebastian@hiebl.cc>
2020-01-29 18:43:16 +0100

    add quadrant logic

--- detect.py
+++ detect.py
@@ -1,40 +1,107 @@
 import cv2
 import numpy as np
 
-def detect(path):
+IMG_WIDTH = 400
+IMG_HEIGHT = 300
+
+def get_small_img(path):
     img = cv2.imread(path)
-    # resize = cv2.resize(img, None, fx=0.3, fy=0.3, interpolation = cv2.INTER_CUBIC)
-    resize = cv2.resize(img, (400, 300), interpolation = cv2.INTER_CUBIC)
-    gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
+    return cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT), interpolation = cv2.INTER_CUBIC)
+    
+def detect_straight_lines(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _,thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
-    # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    blurred = cv2.blur(thresh, (11, 11))
 
-    # cv2.drawContours(resize, contours, -1, (0, 255, 0), 3)
+    traced = cv2.Canny(blurred, 50, 200, None, 3)
 
-    # Copy edges to the images that will display the results in BGR
-    dst = cv2.Canny(thresh, 100, 180, None, 3)
+    # cdstP = cv2.cvtColor(traced, cv2.COLOR_GRAY2BGR)
 
-    cdstP = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    lines = cv2.HoughLinesP(traced, rho = 1, theta = 1*np.pi/180, threshold = 70, minLineLength = 150, maxLineGap = 200)
 
-    linesP = cv2.HoughLinesP(dst, rho = 1, theta = 1*np.pi/180, threshold = 100, minLineLength = 150, maxLineGap = 200)
+    if lines is not None:
+        for i in range(0, len(lines)):
+            l = lines[i][0]
+            cv2.line(img, (l[0], l[1]), (l[2], l[3]), (0,0,255), 1, cv2.LINE_AA)
 
-    if linesP is not None:
-        for i in range(0, len(linesP)):
-            l = linesP[i][0]
-            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
-
-
-    # cv2.imshow('Calculated Output', thresh)
-    # cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
-    threshrgb = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
-    imstack = np.hstack((threshrgb, cdstP))
+    tracedrgb = cv2.cvtColor(traced, cv2.COLOR_GRAY2BGR)
+    imstack = np.hstack((img, tracedrgb))
     cv2.imshow(path, imstack)
+    return lines
 
+def detect_box(name, lines):
+    quadrants = seperate_lines(lines)
+    print_aligned_lines(name, quadrants)
 
-detect('./samples/kasten1.jpg')
-detect('./samples/kasten2.jpg')
-detect('./samples/kasten3.jpg')
-detect('./samples/kasten4.jpg')
-detect('./samples/kasten5.jpg')
-detect('./samples/kasten6.jpg')
+#   +-------+-------+            I 
+#   |       |       |        ----------
+#   |   I   |  II   |      |            |
+#   |       |       |      |            |
+#   +-------+-------+   IV |            | II
+#   |       |       |      |            |   
+#   |  IV   |  III  |      |            |   
+#   |       |       |       -----------     
+#   +-------+-------+           III         
+def seperate_lines(lines):
+    ret = ([], [], [], [])
+    for line in lines:
+        line = line[0]
+        quadrants = (get_quadrant_of_point((line[0], line[1])), get_quadrant_of_point((line[2], line[3])))
+        alignment = get_line_alignment(quadrants)
+        if alignment == 0:
+            continue
+        ret[alignment - 1].append(line)
+    return ret
+
+def get_quadrant_of_point(point):
+    if point[0] < IMG_WIDTH / 2:
+        if point[1] < IMG_HEIGHT / 2:
+            return 1
+        else:
+            return 4
+    else:
+        if point[1] < IMG_HEIGHT / 2:
+            return 2
+        else:
+            return 3
+
+def get_line_alignment(quadrants):
+    quadset = set(quadrants)
+    if quadset == {1,2}:
+        return 1
+    if quadset == {2,3}:
+        return 2
+    if quadset == {3,4}:
+        return 3
+    if quadset == {4,1}:
+        return 4
+    return 0
+
+def print_aligned_lines(name, quadrants):
+    empty = np.zeros((IMG_HEIGHT, IMG_WIDTH, 3), np.uint8)
+    for line in quadrants[0]:
+        cv2.line(empty, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 1, cv2.LINE_AA)
+    for line in quadrants[1]:
+        cv2.line(empty, (line[0], line[1]), (line[2], line[3]), (255, 0, 255), 1, cv2.LINE_AA)
+    for line in quadrants[2]:
+        cv2.line(empty, (line[0], line[1]), (line[2], line[3]), (0, 255, 255), 1, cv2.LINE_AA)
+    for line in quadrants[3]:
+        cv2.line(empty, (line[0], line[1]), (line[2], line[3]), (255, 255, 0), 1, cv2.LINE_AA)
+    cv2.imshow(name, empty)
+
+paths = [
+    './samples/kasten1.jpg',
+    './samples/kasten2.jpg',
+    './samples/kasten3.jpg',
+    './samples/kasten4.jpg',
+    './samples/kasten5.jpg',
+    './samples/kasten6.jpg',
+]
+
+for path in paths:
+    print(path)
+    resized = get_small_img(path)
+    lines = detect_straight_lines(resized)
+    shape = detect_box(path, lines)
+
 cv2.waitKey()
